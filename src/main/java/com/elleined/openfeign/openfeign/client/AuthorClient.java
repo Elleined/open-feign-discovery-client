3c7e555ccb4e9f246e00340c83fcdfaca5388f1b
Elleined <demadegu@gmail.com>
2023-07-15 17:30:49 +0800

    Added all my book-author-genre-api in my feign client

--- src/main/java/com/elleined/openfeign/openfeign/client/AuthorClient.java
+++ src/main/java/com/elleined/openfeign/openfeign/client/AuthorClient.java
@@ -2,12 +2,52 @@ package com.elleined.openfeign.openfeign.client;
 
 import com.elleined.openfeign.openfeign.dto.AuthorDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient(name = "author-api", url = "http://localhost:8080/api/v1/authors")
 public interface AuthorClient {
     @GetMapping
-    List<AuthorDTO> getAll();
+    List<AuthorDTO> getAll() ;
+
+    @GetMapping("/get-all-by-id")
+    List<AuthorDTO> getAllById(@RequestParam("ids") List<Integer> authorIds);
+
+    @GetMapping("/{id}")
+    AuthorDTO getById(@PathVariable("id") int authorId) ;
+
+    @GetMapping("/{id}/books")
+    List<String> getAllBooks(@PathVariable("id") int authorId) ;
+
+    @GetMapping("/{id}/books/count")
+    int getBookCount(@PathVariable("id") int authorId) ;
+
+    @GetMapping("/name")
+    List<String> searchByFirstLetter(@RequestParam("firstLetter") char firstLetter) ;
+
+    @GetMapping("/name/{name}")
+    AuthorDTO getByName(@PathVariable("name") String authorName) ;
+
+    @GetMapping("/{pageNumber}/{pageSize}")
+    List<AuthorDTO> getAll(@PathVariable int pageNumber,
+                           @PathVariable int pageSize);
+
+    @GetMapping("/{pageNumber}/{pageSize}/{sortDirection}/{sortProperty}")
+    List<AuthorDTO> getAll(@PathVariable int pageNumber,
+                           @PathVariable int pageSize,
+                           @PathVariable String sortDirection,
+                           @PathVariable String sortProperty);
+
+    @PostMapping
+    ResponseEntity<?> save(@RequestBody AuthorDTO authorDTO);
+
+    @PostMapping("/save-all")
+    ResponseEntity<?> saveAll (@RequestBody List<AuthorDTO> authors);
+
+    @PutMapping("/{id}")
+    ResponseEntity<?> update(@PathVariable("id") int authorId,
+                             @RequestBody AuthorDTO authorDTO);
 }
--- src/main/java/com/elleined/openfeign/openfeign/controller/AuthorController.java
+++ src/main/java/com/elleined/openfeign/openfeign/controller/AuthorController.java
@@ -3,9 +3,8 @@ package com.elleined.openfeign.openfeign.controller;
 import com.elleined.openfeign.openfeign.client.AuthorClient;
 import com.elleined.openfeign.openfeign.dto.AuthorDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,9 +13,68 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthorController {
     private final AuthorClient authorClient;
-
     @GetMapping
-    public List<AuthorDTO> getAll() {
+    List<AuthorDTO> getAll()  {
         return authorClient.getAll();
+    }
+
+    @GetMapping("/get-all-by-id")
+    List<AuthorDTO> getAllById(@RequestParam("ids") List<Integer> authorIds) {
+        return authorClient.getAllById(authorIds);
+    }
+
+    @GetMapping("/{id}")
+    AuthorDTO getById(@PathVariable("id") int authorId)  {
+        return authorClient.getById(authorId);
+    }
+
+    @GetMapping("/{id}/books")
+    List<String> getAllBooks(@PathVariable("id") int authorId)  {
+        return authorClient.getAllBooks(authorId);
+    }
+
+    @GetMapping("/{id}/books/count")
+    int getBookCount(@PathVariable("id") int authorId)  {
+        return authorClient.getBookCount(authorId);
+    }
+
+    @GetMapping("/name")
+    List<String> searchByFirstLetter(@RequestParam("firstLetter") char firstLetter)  {
+        return authorClient.searchByFirstLetter(firstLetter);
+    }
+
+    @GetMapping("/name/{name}")
+    AuthorDTO getByName(@PathVariable("name") String authorName)  {
+        return authorClient.getByName(authorName);
+    }
+
+    @GetMapping("/{pageNumber}/{pageSize}")
+    List<AuthorDTO> getAll(@PathVariable int pageNumber,
+                           @PathVariable int pageSize) {
+        return authorClient.getAll(pageNumber, pageSize);
+    }
+
+    @GetMapping("/{pageNumber}/{pageSize}/{sortDirection}/{sortProperty}")
+    List<AuthorDTO> getAll(@PathVariable int pageNumber,
+                           @PathVariable int pageSize,
+                           @PathVariable String sortDirection,
+                           @PathVariable String sortProperty) {
+        return authorClient.getAll(pageNumber, pageSize, sortDirection, sortProperty);
+    }
+
+    @PostMapping
+    ResponseEntity<?> save(@RequestBody AuthorDTO authorDTO) {
+        return authorClient.save(authorDTO);
+    }
+
+    @PostMapping("/save-all")
+    ResponseEntity<?> saveAll (@RequestBody List<AuthorDTO> authors) {
+        return authorClient.saveAll(authors);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<?> update(@PathVariable("id") int authorId,
+                             @RequestBody AuthorDTO authorDTO) {
+        return authorClient.update(authorId, authorDTO);
     }
 }
