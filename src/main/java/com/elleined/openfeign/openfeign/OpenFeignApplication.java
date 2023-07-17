2c8b7b6d4aa567ade73eb7e049c8b2044d45bbda
Elleined <demadegu@gmail.com>
2023-07-17 22:39:11 +0800

    Added eureka client dependency and enabling autoconfiguration for eureka client

--- src/main/java/com/elleined/openfeign/openfeign/OpenFeignApplication.java
+++ src/main/java/com/elleined/openfeign/openfeign/OpenFeignApplication.java
@@ -2,10 +2,12 @@ package com.elleined.openfeign.openfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OpenFeignApplication {
 
 	public static void main(String[] args) {
